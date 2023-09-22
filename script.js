2ad8c84b95f6890972cb48e1afd53447dd746087
João <joaoalbertobraun@gmail.com>
2023-09-22 12:22:44 -0300

    logo changed, delete some comments in js file

--- script.js
+++ script.js
@@ -50,34 +50,3 @@ highlightBtnVideo.addEventListener("click", function (e) {
 		youtube.style.display = "none";
 	}
 });
-
-/*
-//for future implementation
-//sticky nav
-const header = document.querySelector(".header");
-const navHeight = nav.getBoundingClientRect().height;
-
-const stickyNav = function (entries) {
-	const [entry] = entries;
-
-	if (!entry.isIntersecting) nav.classList.add("sticky");
-	else nav.classList.remove("sticky");
-};
-
-const headerObserver = new IntersectionObserver(stickyNav, {
-	root: null,
-	threshold: 0,
-	rootMargin: `-${navHeight}px`,
-});
-
-headerObserver.observe(header);
-
-const revealSection = function (entries, observer) {
-	const [entry] = entries;
-
-	if (!entry.isIntersecting) return;
-
-	entry.target.classList.remove("section--hidden");
-	observer.unobserve(entry.target);
-};
-*/
